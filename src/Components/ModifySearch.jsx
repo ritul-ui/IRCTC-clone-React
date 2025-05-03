1b9eeb7a484ccdfb9f18852b43828e307e84f94c
ritul-ui <ritulanand1996@gmail.com>
2025-05-04 00:54:04 +0530

    deployment fix-4

--- src/Components/ModifySearch.jsx
+++ src/Components/ModifySearch.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import styles from "../styles/ModifySearch.module.scss";
+import styles from "../Styles/ModifySearch.module.scss";
 import { FaExchangeAlt } from "react-icons/fa";
 
 const ModifySearch = () => {
